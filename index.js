4163afbc1f4b5afb8b09d80da3d08c83f5035005
Mugen <melton.sheridan@gmail.com>
2022-05-22 22:49:24 -0400

    added functions to add employee/role/update/and department but employee and update are bugged

--- index.js
+++ index.js
@@ -52,12 +52,12 @@ function menuPrompt() {
                             addDepartment();
                             break;
 
-                            case 'Add Employees':
-                                addEmployees();
+                            case 'Add Employee':
+                                addEmployee();
                                 break;
 
-                                case 'Add Roles':
-                                    addRoles();
+                                case 'Add Role':
+                                    addRole();
                                     break;
 
                                     case 'Update Employee Role':
@@ -96,3 +96,175 @@ function viewRoles() {
         menuPrompt()
     })
 }
+
+// if user wants to add a department
+function addDepartment() { 
+
+    inquirer.prompt([
+        {
+          name: "name",
+          type: "input",
+          message: "What Department would you like to add?"
+        }
+    ]).then(function(res) {
+        var query = connect.query(
+            "INSERT INTO department SET ? ",
+            {
+              name: res.name
+            
+            },
+            function(err) {
+                if (err) throw err
+                console.table(res);
+                menuPrompt();
+            }
+        )
+    })
+}
+// this creates an array for the roles that is being grabbed from role table
+let rolesArray = [];
+function chooseRole() {
+    connect.query("SELECT FROM role", function(err, res) {
+        if (err) throw err
+        for ( var i = 0; i < res.length; i++) {
+            rolesArray.push(res[i].title);
+        }
+    })
+    return rolesArray;
+}
+// this creates an array for managers being grabbed from employee table
+let managerArray = [];
+function chooseManager() {
+    connect.query("SELECT * FROM employee WHERE manager_id IS NULL", 
+    function(err, res) {
+        if (err) throw err
+        for (var i = 0; i < res.length; i++) {
+            managerArray.push(res[i].first_name);
+        }
+    })
+    return managerArray;
+}
+
+// this prompts questions to add employee
+function addEmployee() {
+    inquirer.prompt([
+        {
+            name: 'first name',
+            type: 'input',
+            message: "What is your new employee's first name?"
+        },
+        {
+            name: 'last name',
+            type: 'input',
+            message: "What is your new employee's last name?"
+        },
+        {
+            name: 'role',
+            type: 'list',
+            message:"Please choose a role.",
+            choices: chooseRole()
+        },
+        {
+            name: 'choice',
+            type: 'rawlist',
+            message: "What is their managers name?",
+            choices: chooseManager()
+        }
+    ]).then(function (select) {
+        let roleId = chooseRole().indexOf(select.role) + 1
+        let managerId = chooseManager().indexOf(select.choice) + 1
+        connect.query("INSERT INTO employee SET ?", 
+        {
+            first_name: select.firstName,
+            last_name: select.lastName,
+            manager_id: managerId,
+            role_id: roleId
+            
+        }, function(err){
+            if (err) throw err
+            console.table(select)
+            menuPrompt()
+        })
+  
+    })
+  }
+
+  // if user selects update employee
+  function updateEmployeeRole() {
+      connect.query("SELECT * FROM employee",
+      function(err, res) {
+          if (err) throw err
+          console.log(res)
+          inquirer.prompt([
+              {
+                  name: 'lastName',
+                  type: 'rawlist',
+                  choices: function() {
+                      let lastName = [];
+                      for (var i = 0; i < res.length; i++) {
+                          lastName.push(res[i].last_name);
+                      }
+                      return lastName;
+                  },
+                  message: "What is your employee's last name?",
+              },
+              {
+                  name: 'role',
+                  type: 'rawlist',
+                  message: "What is your employee's new role?",
+                  choices: chooseRole()
+              },
+          ]).then(function(select) {
+            let roleId = chooseRole().indexOf(select.role) + 1
+            connect.query("UPDATE employee SET WHERE ?", 
+            {
+              last_name: select.lastName
+               
+            }, 
+            {
+              role_id: roleId
+               
+            }, 
+            function(err){
+                if (err) throw err
+                console.table(select)
+                menuPrompt()
+            })
+      
+        });
+      });
+    
+      }
+
+      // if user selects add new role
+      function addRole() { 
+        connect.query("SELECT role.title AS Title, role.salary AS Salary FROM role",   function(err, res) {
+          inquirer.prompt([
+              {
+                name: "Title",
+                type: "input",
+                message: "What is the roles Title?"
+              },
+              {
+                name: "Salary",
+                type: "input",
+                message: "What is the Salary?"
+      
+              } 
+          ]).then(function(res) {
+              connect.query(
+                  "INSERT INTO role SET ?",
+                  {
+                    title: res.Title,
+                    salary: res.Salary,
+                  },
+                  function(err) {
+                      if (err) throw err
+                      console.table(res);
+                      menuPrompt();
+                  }
+              )
+      
+          });
+        });
+        }
